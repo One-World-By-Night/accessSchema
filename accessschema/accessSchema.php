95825fead49661ba0e3de0164a30bbb8934bf134
Greg Hacke <greghacke@rskgroup.org>
2025-06-10 11:01:06 -0400

    1.1.0 -Client Documentation

--- accessschema/accessSchema.php
+++ accessschema/accessSchema.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: accessSchema
  * Description: Manage Role-based access schema plugin with audit logging and REST API support.
- * Version: 1.0.5
+ * Version: 1.1.0
  * Author: greghacke
  * Author URI: https://www.owbn.net
  * Text Domain: accessschema
