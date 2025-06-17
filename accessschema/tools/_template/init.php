eacee861de6db3d632002833a6f3d7ba6597bbcc
Greg Hacke <greghacke@rskgroup.org>
2025-06-17 16:22:31 -0400

    templating tools

--- accessschema/tools/_template/init.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// File: tools/_template/init.php
-// @version 1.6.0
-// @author greghacke
-// @tool _template
-
-defined( 'ABSPATH' ) || exit;
-
---
+++ accessschema/tools/_template/wp-plugin-name.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name: WP Plugin Nam
+ * Text Domain: wp-plugin-name
+ * Plugin URI: https://www.github.com/One-World-By-Night/wp-plugin-name
+ * Description: Short description of the plugin functionality.
+ * Version: 1.0.0
+ * Author: list, of, authors, separated, by, commas
+ * Contributors: list, of, contributors, separated, by, commas
+ * Author URI: https://www.github.com/One-World-By-Night
+ * License: GPL-2.0-or-later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ * Domain Path: /includes/languages
+ * GitHub Branch: main
+ */
+
+ defined('ABSPATH') || exit;
+
+require_once __DIR__ . '/includes/init.php';
