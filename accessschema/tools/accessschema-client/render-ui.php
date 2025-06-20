ca4801964446847e4edd6db1327f943d238fa5cd
Greg Hacke <greghacke@rskgroup.org>
2025-06-20 12:22:07 -0400

    Remove client (now independent repo)

--- accessschema/tools/accessschema-client/render-ui.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// File: accessschema-client/render-ui.php
-// @version 1.6.0
-// @author greghacke
-// @tool accessschema-client
-
-defined( 'ABSPATH' ) || exit;
-
--- accessschema/tools/accessschema-client/webhook.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// File: accessschema-client/webhook.php
-// @version 1.6.0
-// @author greghacke
-// @tool accessschema-client
-
-defined( 'ABSPATH' ) || exit;
-
